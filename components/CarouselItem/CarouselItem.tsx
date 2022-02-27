68ce465d3ae485b6958be17a021b2c0c3aeb705e
Adeola Akinsiku <adeola.ak@outlook.com>
2022-02-27 15:54:14 -0500

    removed conditional render of carouselitem

--- components/CarouselItem/CarouselItem.tsx
+++ components/CarouselItem/CarouselItem.tsx
@@ -1,4 +1,3 @@
-import EmployeeCard from "../EmployeeCard/EmployeeCard";
 import styles from "../CarouselItem/CarouselItem.module.css";
 
 interface CarouselItemProps {
@@ -7,45 +6,44 @@ interface CarouselItemProps {
 }
 
 const CarouselItem = ({ level, employee }: CarouselItemProps) => {
-  if (employee !== undefined) {
-    return (
-      <>
-        <div className={styles.carouselItem + " " + styles["level" + level]}>
-          <div className={styles.carouselItemContent}>
-            <div className={styles.employeePhotoContainer}>
-              <img src={employee.photoUrl} className={styles.employeePhoto} />
-            </div>
-            <div className={styles.employeeInfo}>
-              <span className={styles.employeeInfoText}>
-                {employee.preferredName
-                  ? employee.preferredName + " " + employee.lastName
-                  : employee.firstName + " " + employee.lastName}
-                {employee.customPronouns
-                  ? "," + " " + employee.customPronouns
-                  : null}
-              </span>
+  // if (employee !== undefined) {
+  return (
+    <>
+      <div className={styles.carouselItem + " " + styles["level" + level]}>
+        <p>{employee.name}</p>
 
-              <br></br>
-              <span className={styles.employeeInfoText}>
-                {employee.department}
-              </span>
+        <div className={styles.carouselItemContent}>
+          <div className={styles.employeePhotoContainer}>
+            <img src={employee.photoUrl} className={styles.employeePhoto} />
+          </div>
+          <div className={styles.employeeInfo}>
+            <span className={styles.employeeInfoText}>
+              {employee.preferredName
+                ? employee.preferredName + " " + employee.lastName
+                : employee.firstName + " " + employee.lastName}
+              {employee.customPronouns
+                ? "," + " " + employee.customPronouns
+                : null}
+            </span>
 
-              <span className={styles.employeeInfoText}>
-                {employee.jobTitle}
-              </span>
+            <br></br>
+            <span className={styles.employeeInfoText}>
+              {employee.department}
+            </span>
 
-              <span className={styles.employeeInfoText}>
-                Start Date {employee.hireDate}
-              </span>
+            <span className={styles.employeeInfoText}>{employee.jobTitle}</span>
 
-              <br></br>
-              <span className={styles.employeeInfoText}>hashtags </span>
-            </div>
+            <span className={styles.employeeInfoText}>
+              Start Date {employee.hireDate}
+            </span>
+
+            <br></br>
+            <span className={styles.employeeInfoText}>hashtags </span>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 };
 
 export default CarouselItem;

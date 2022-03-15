6aae5fc3e12acaac374047fd0ea9c86d8e494a0c
Adeola Akinsiku <adeola.ak@outlook.com>
2022-03-15 00:34:19 -0400

    styles cleanup4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,45 @@ import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
 import { Provider } from "next-auth/client";
+
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar/Navbar";
+import Image from "next/image";
+import logo from "../assets/sg_logo.png";
+
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-config.autoAddCss = false;
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  config.autoAddCss = false;
+
   return (
     <Provider session={pageProps.session}>
+      <Head>
+        <title>Humans of Sourcegraph</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+      <Navbar />
+      <div className={styles.logo}>
+        <Link href="/">
+          <Image
+            className={styles.imgComponent}
+            src={logo}
+            alt="Sourcegraph logo"
+            width={320}
+            height={54}
+          />
+        </Link>
+      </div>
+      <div className={styles.pageDescription}>
+        <p className={styles.heavyText}>Humans of Sourcegraph</p>
+        {/* <p className={styles.mutedText}>
+          Familiarize yourself with our teammates and organization.
+        </p> */}
+      </div>
+
       <Component {...pageProps} />
     </Provider>
   );
--- pages/explore.tsx
+++ pages/explore.tsx
@@ -1,7 +1,57 @@
-import Globe from "../components/Globe/Globe";
+interface ExploreProps {
+  employeeLocations: { [key: string]: any };
+}
 
-const Explore = () => {
-  return <div>{/* <Globe /> */}</div>;
+const Explore = ({ employeeLocations }: ExploreProps) => {
+  console.log(employeeLocations);
+
+  //   const countries = employeeLocations.map((employee) => {
+  //     const country = employee.country;
+  //     const test = new Set(country);
+  //     return <div>{test}</div>;
+  //   });
+  //   return <div>{countries}</div>;
+  // };
+
+  return <div>Coming soon</div>;
 };
+
+export async function getServerSideProps() {
+  const locationOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Basic ${process.env.API_KEY}`,
+    },
+    body: JSON.stringify({
+      fields: [
+        "firstName",
+        "preferredName",
+        "lastName",
+        "photoUploaded",
+        "photoUrl",
+        "country",
+        "status",
+      ],
+    }),
+  };
+  const employeeLocations = await fetch(
+    "https://api.bamboohr.com/api/gateway.php/sourcegraph/v1/reports/custom?format=JSON",
+    locationOptions,
+  )
+    .then((response) => response.json())
+    .then((data) =>
+      data.employees.filter(
+        (employee: { [key: string]: any }) => employee.status != "Inactive",
+      ),
+    )
+    .catch((err) => console.error(err));
+
+  return {
+    props: {
+      employeeLocations,
+    },
+  };
+}
 
 export default Explore;
